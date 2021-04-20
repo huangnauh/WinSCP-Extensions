57aed72bbbb029fefedd666dcd1fb0e3978d2639
huangnauh <huanglibo2010@gmail.com>
2021-04-20 16:40:49 +0800

    hostmap support csv

--- GenerateHttpUrl.WinSCPextension.ps1
+++ GenerateHttpUrl.WinSCPextension.ps1
@@ -15,8 +15,8 @@
 # @option         Https -site checkbox "Use HTTP&S" "" "-https"
 # @option         RootPath -site textbox "&URL root path (optional):"
 # @option         HostName -site textbox "&Web server hostname override (optional):"
-# @option         HostMap -config file "S3 空间和主机对应列表，以分号分割 (可选):" ^
-#                     "C:\Program Files\WinSCP\HostMap.txt"
+# @option         HostMap -config file "S3 空间和主机对应列表，以 csv 格式保存:" ^
+#                     "C:\Program Files\WinSCP\HostMap.csv"
 # @option       - group "Options"
 # @option         Pause checkbox "Display URL" "-pause" "-pause"
 # @option         Clipboard checkbox "Copy URL to clipboard" "-clipboard" "-clipboard"
@@ -95,8 +95,17 @@ try
 
         if (!$hostName -and $hostMap)
         {
+            if (!(Test-Path -Path $hostMap -PathType Leaf))
+            {
+                throw "**The file $hostMap is not exist.**"
+            }
             Get-Content -Path $hostMap | ForEach-Object {
-                $splits = ($_ -split ":")
+                $splits = ($_ -split ",")
+                if ($splits.Length -ne 2)
+                {
+                    throw "**Contents of the file $hostMap is invalid.**"
+                }
+
                 if ($splits[0] -eq $pathSplit[1])
                 {
                     $hostName = $splits[1]
